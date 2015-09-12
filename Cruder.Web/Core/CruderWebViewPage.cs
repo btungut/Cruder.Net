8420250a77cf24550cdb5b11cafe38cc9eca5983
Burak TUNGUT <info@buraktungut.com>
2015-09-13 00:44:09 +0300

    Custom WebViewPage class added.

---
+++ Cruder.Web/Core/CruderWebViewPage.cs
@@ -0,0 +1,41 @@
+﻿using Cruder.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cruder.Web
+{
+    public class CruderWebViewPage : System.Web.Mvc.WebViewPage
+    {
+        public CruderHtmlHelper CruderHtml { get; set; }
+
+        public override void InitHelpers()
+        {
+            base.InitHelpers();
+
+            CruderHtml = new CruderHtmlHelper(base.Html);
+        }
+
+        public override void Execute()
+        {
+        }
+    }
+
+    public class CruderWebViewPage<TModel> : System.Web.Mvc.WebViewPage<TModel>
+    {
+        public CruderHtmlHelper<TModel> CruderHtml { get; set; }
+
+        public override void InitHelpers()
+        {
+            base.InitHelpers();
+
+            CruderHtml = new CruderHtmlHelper<TModel>(base.Html);
+        }
+
+        public override void Execute()
+        {
+        }
+    }
+}
